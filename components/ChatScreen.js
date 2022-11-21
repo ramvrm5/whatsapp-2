07ea77df129c559451b39f15fd3d869ffee2118a
ram verma <42866242+ramvrm5@users.noreply.github.com>
2022-11-21 13:20:20 +0530

    changes in file chatScreen.js chnages in file

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -51,7 +51,6 @@ function ChatScreen({ chat, messages }) {
 
     const sendMesssage = (e) => {
         e.preventDefault();
-
         db.collection('users').doc(user.uid).set(
             {
                 lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
